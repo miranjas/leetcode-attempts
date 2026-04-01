c88d49b72eb408e8e096373a6bc3a627d1faa9db
Jason Miranda <miranjas@mail.gvsu.edu>
2026-04-01 13:09:08 -0400

    lc 14

---
+++ leetcode-14.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        prefix = ""
+        # setting shortest word from strs
+        # anything longer cant count as a common prefix
+        shortest = min(strs,key=len)
+        if len(strs) == 0:
+            return prefix
+        
+        # iterating through letters in shortest word and comparing to the same index in all other words
+        for i in range(len(shortest)):
+            for words in strs:
+                # if different letter at the index, return what we have
+                if shortest[i] != words[i]:
+                    return prefix
+            prefix += words[i]
+        return prefix
+
