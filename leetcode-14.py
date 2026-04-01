77953bf6bb2e63836826f10c5b2c3dbdc5f5bc94
Jason Miranda <miranjas@mail.gvsu.edu>
2026-04-01 13:10:03 -0400

    small typo

--- leetcode-14.py
+++ leetcode-14.py
@@ -1,5 +1,5 @@
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
         prefix = ""
         # setting shortest word from strs
         # anything longer cant count as a common prefix
